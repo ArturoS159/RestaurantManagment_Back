504f747f1a7c1a57eeb042ad3cc557cb2da1ffb5
ArturoS159 <akowalski3677@gmail.com>
2020-11-08 19:00:34 +0100

    fix cors

--- resource-server/src/main/java/com/przemarcz/auth/ResourceController.java
+++ resource-server/src/main/java/com/przemarcz/auth/ResourceController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.security.Principal;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 class ResourceController {
 
     @GetMapping("/me")
