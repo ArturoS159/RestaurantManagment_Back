6c94ca1a4dd6b5fcc7b4fb9620a6471507c40155
ArturoS159 <akowalski3677@gmail.com>
2020-11-10 10:52:07 +0100

    add more category to restaurant

--- restaurant-service/src/main/java/com/przemarcz/restaurant/model/enums/RestaurantCategory.java
+++ restaurant-service/src/main/java/com/przemarcz/restaurant/model/enums/RestaurantCategory.java
@@ -1,5 +1,5 @@
 package com.przemarcz.restaurant.model.enums;
 
 public enum RestaurantCategory {
-    STREET_FOOD, SUSHI
+    STREET_FOOD, SUSHI, PIZZA, BURGER, PASTA, KEBAB, VEGE, FIT, FISH, SEAFOOD, THAI_CUISINE, TURKISH_CUISINE, POLISH_CUISINE, MAXICAN_CUISINE, VIETNAMSE_CUISINE
 }
