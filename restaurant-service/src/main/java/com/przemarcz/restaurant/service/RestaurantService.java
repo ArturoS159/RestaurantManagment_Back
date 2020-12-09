fa49a1ed4d0a12c0266b94b71fd2917c8243fb00
ArturoS159 <akowalski3677@gmail.com>
2020-12-09 13:27:52 +0100

    fix imports

--- restaurant-service/src/main/java/com/przemarcz/restaurant/service/RestaurantService.java
+++ restaurant-service/src/main/java/com/przemarcz/restaurant/service/RestaurantService.java
@@ -21,7 +21,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 import java.util.UUID;
