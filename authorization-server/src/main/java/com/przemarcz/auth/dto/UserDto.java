7ad9ead6d707d2aeb66a5364b378277a44855480
ArturoS159 <akowalski3677@gmail.com>
2020-12-14 21:17:25 +0100

    hotfix max to size max

--- authorization-server/src/main/java/com/przemarcz/auth/dto/UserDto.java
+++ authorization-server/src/main/java/com/przemarcz/auth/dto/UserDto.java
@@ -11,31 +11,31 @@ public class UserDto {
     public static class RegisterUserRequest {
         @NotBlank(message = "Email must be not blank")
         @Email(message = "It's not email!")
-        @Max(value = 50, message = "Email must be lower than 50")
+        @Size(max = 50, message = "Email must be lower than 50")
         String email;
         @NotBlank(message = "Password must be not blank")
         @Size(min = 4, max = 50, message = "Password must be in range! 4-50")
         String password;
         @NotBlank(message = "Login must be not blank")
-        @Max(value = 50, message = "Login must be lower than 50")
+        @Size(max = 50, message = "Login must be lower than 50")
         String login;
     }
 
     @Value
     public static class UpdateUserRequest {
-        @Max(value = 50, message = "Forename must be lower than 50")
+        @Size(max = 50, message = "Forename must be lower than 50")
         String forename;
-        @Max(value = 50, message = "Surname must be lower than 50")
+        @Size(max = 50, message = "Surname must be lower than 50")
         String surname;
-        @Max(value = 50, message = "Street must be lower than 50")
+        @Size(max = 50, message = "Street must be lower than 50")
         String street;
-        @Max(value = 50, message = "City must be lower than 50")
+        @Size(max = 50, message = "City must be lower than 50")
         String city;
         @Pattern(regexp = "\\d{2}-\\d{3}", message = "It's not postcode")
         String postCode;
         @Pattern(regexp = "(?<!\\w)(\\(?(\\+|00)?48\\)?)?[ -]?\\d{3}[ -]?\\d{3}[ -]?\\d{3}(?!\\w)", message = "It's not phone number")
         String phoneNumber;
-        @Max(value = 20, message = "House number must be lower then 50")
+        @Size(max = 20, message = "House number must be lower then 20")
         String houseNumber;
     }
 
--- restaurant-service/src/main/java/com/przemarcz/restaurant/dto/OrderDto.java
+++ restaurant-service/src/main/java/com/przemarcz/restaurant/dto/OrderDto.java
@@ -17,20 +17,20 @@ public class OrderDto {
     @Value
     public static class CreateOrderUserRequest {
         @NotBlank(message = "Forename must be not blank")
-        @Max(value = 50, message = "Forename be lower than 50")
+        @Size(max = 50, message = "Forename be lower than 50")
         String forename;
         @NotBlank(message = "Surname must be not blank")
-        @Max(value = 50, message = "Surname mbe lower than 50")
+        @Size(max = 50, message = "Surname mbe lower than 50")
         String surname;
         @NotBlank(message = "Street must be not blank")
-        @Max(value = 50, message = "Street be lower than 50")
+        @Size(max = 50, message = "Street be lower than 50")
         String street;
         @NotBlank(message = "City must be not blank")
-        @Max(value = 50, message = "City must be lower than 50")
+        @Size(max = 50, message = "City must be lower than 50")
         String city;
         @NotBlank(message = "Email must be not blank")
         @Email(message = "It's not email!")
-        @Max(value = 50, message = "Email must be lower than 50")
+        @Size(max = 50, message = "Email must be lower than 50")
         String email;
         @NotBlank(message = "Phone-number must be not blank")
         @Pattern(regexp = "(?<!\\w)(\\(?(\\+|00)?48\\)?)?[ -]?\\d{3}[ -]?\\d{3}[ -]?\\d{3}(?!\\w)", message = "It's not phone number")
@@ -38,7 +38,7 @@ public class OrderDto {
         @NotBlank(message = "Surname must be not blank")
         @Pattern(regexp = "\\d{2}-\\d{3}", message = "It's not postcode")
         String postCode;
-        @Max(value = 300, message = "Comment must be lower than 300")
+        @Size(max = 300, message = "Comment must be lower than 300")
         String comment;
         @NotNull(message = "Order-type must be not null")
         OrderType orderType;
@@ -50,22 +50,22 @@ public class OrderDto {
 
     @Value
     public static class CreateOrderPersonalRequest {
-        @Max(value = 50, message = "Forename must be lower than 50")
+        @Size(max = 50, message = "Forename must be lower than 50")
         String forename;
-        @Max(value = 50, message = "Surname mbe lower than 50")
+        @Size(max = 50, message = "Surname mbe lower than 50")
         String surname;
-        @Max(value = 50, message = "Street be lower than 50")
+        @Size(max = 50, message = "Street be lower than 50")
         String street;
-        @Max(value = 50, message = "City must be lower than 50")
+        @Size(max = 50, message = "City must be lower than 50")
         String city;
         @Email(message = "It's not email!")
-        @Max(value = 50, message = "Email must be lower than 50")
+        @Size(max = 50, message = "Email must be lower than 50")
         String email;
         @Pattern(regexp = "(?<!\\w)(\\(?(\\+|00)?48\\)?)?[ -]?\\d{3}[ -]?\\d{3}[ -]?\\d{3}(?!\\w)", message = "It's not phone number")
         String phoneNumber;
         @Pattern(regexp = "\\d{2}-\\d{3}", message = "It's not postcode")
         String postCode;
-        @Max(value = 300, message = "Comment must be lower than 300")
+        @Size(max = 300, message = "Comment must be lower than 300")
         String comment;
         @NotNull(message = "Order-type must be not null")
         OrderType orderType;
