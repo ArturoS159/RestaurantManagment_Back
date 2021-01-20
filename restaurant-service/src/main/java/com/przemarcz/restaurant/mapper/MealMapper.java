ec3d23559ba0462c3b01766c149fb33fc0b6232d
ArturoS159 <akowalski3677@gmail.com>
2021-01-20 19:30:22 +0100

    refactor category to first upper letter

--- restaurant-service/src/main/java/com/przemarcz/restaurant/mapper/MealMapper.java
+++ restaurant-service/src/main/java/com/przemarcz/restaurant/mapper/MealMapper.java
@@ -4,6 +4,7 @@ import com.przemarcz.restaurant.dto.MealDto.CreateMealRequest;
 import com.przemarcz.restaurant.dto.MealDto.MealResponse;
 import com.przemarcz.restaurant.dto.MealDto.UpdateMealRequest;
 import com.przemarcz.restaurant.model.Meal;
+import org.apache.commons.lang3.StringUtils;
 import org.mapstruct.*;
 
 import java.math.BigDecimal;
@@ -12,12 +13,12 @@ import java.util.UUID;
 
 import static java.util.Objects.isNull;
 
-@Mapper(componentModel = "spring", imports = {UUID.class})
+@Mapper(componentModel = "spring", imports = {UUID.class, StringUtils.class})
 public interface MealMapper {
 
     @Mapping(target = "id", expression = "java(UUID.randomUUID())")
     @Mapping(target = "price", source = "createMealRequest.price", qualifiedByName = "scaleBigDecimal")
-    @Mapping(target = "category", expression = "java(createMealRequest.getCategory().toLowerCase())")
+    @Mapping(target = "category", expression = "java(StringUtils.capitalize(createMealRequest.getCategory().toLowerCase()))")
     Meal toMeal(CreateMealRequest createMealRequest, UUID restaurantId);
 
     @Mapping(target = "price", source = "updateMealRequest.price", qualifiedByName = "scaleBigDecimal")
